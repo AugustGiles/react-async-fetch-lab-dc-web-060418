8f591be23144554628e05c47a21e15468ef07aa8
github username <github email address>
2018-08-07 17:22:54 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  state = {
+    listOfHumans: []
+  }
+
+  componentDidMount() {
+    fetch(`http://api.open-notify.org/astros.json`)
+      .then(resp => resp.json())
+      .then(list => {
+        let names = list.people.map(person => {return person.name})
+        this.setState({listOfHumans: names})
+      })
+      // .then(list => console.log(list.people))
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.listOfHumans.map(human => {
+          return <li>{human}</li>
+        })}
+      </ul>
+    )
+  }
+
+}
+
+export default App;
